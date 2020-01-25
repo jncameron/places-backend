2cdc34161bccaa7a475361f7f8880e49695424f8
John Cameron <john.n.cameron@gmail.com>
2020-01-25 21:36:44 +1100

    add place controller from frontend

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,15 +1,17 @@
-const uuid = require("uuid/v4");
 const { validationResult } = require("express-validator");
 
-const User = require("../models/user");
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const getUsers = async (req, res, next) => {
   let users;
   try {
     users = await User.find({}, "-password");
   } catch (err) {
-    const error = next(new HttpError("Could not find users", 500));
+    const error = new HttpError(
+      "Fetching users failed, please try again later.",
+      500
+    );
     return next(error);
   }
   res.json({ users: users.map(user => user.toObject({ getters: true })) });
@@ -18,20 +20,26 @@ const getUsers = async (req, res, next) => {
 const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(new HttpError("Invalid inputs - check your data", 422));
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
   const { name, email, password } = req.body;
+
   let existingUser;
   try {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
-    const error = new HttpError("Signing up failed. Please try again.", 500);
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
     return next(error);
   }
 
   if (existingUser) {
     const error = new HttpError(
-      "Sorry - that user exists already. Please login.",
+      "User exists already, please login instead.",
       422
     );
     return next(error);
@@ -40,16 +48,19 @@ const signup = async (req, res, next) => {
   const createdUser = new User({
     name,
     email,
+    image: "https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg",
     password,
-    image:
-      "https://i.pinimg.com/474x/6c/2e/50/6c2e50c78952f2fbc073417a61359f36--scary-movies-horror-movies.jpg",
     places: []
   });
 
   try {
     await createdUser.save();
   } catch (err) {
-    new HttpError("Sorry - couldn't signup.", 500);
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
+    return next(error);
   }
 
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
@@ -58,20 +69,30 @@ const signup = async (req, res, next) => {
 const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  let identifiedUser;
+  let existingUser;
+
   try {
-    identifiedUser = await User.findOne({ email: email });
+    existingUser = await User.findOne({ email: email });
   } catch (err) {
-    const error = new HttpError("Signing up failed. Please try again.", 500);
+    const error = new HttpError(
+      "Loggin in failed, please try again later.",
+      500
+    );
     return next(error);
   }
 
-  if (!identifiedUser || identifiedUser.password !== password) {
-    return next(
-      new HttpError("Could not identify user. Incorrect email or password", 401)
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      401
     );
+    return next(error);
   }
-  res.status(200).json({ message: "Logged In!" });
+
+  res.json({
+    message: "Logged in!",
+    user: existingUser.toObject({ getters: true })
+  });
 };
 
 exports.getUsers = getUsers;
